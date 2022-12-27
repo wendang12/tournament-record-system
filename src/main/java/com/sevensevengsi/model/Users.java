3d61fb0ad919ee361e88d4ad1a10ce580b7dbd1e
wendy <mtorrion@77global.biz>
2022-12-27 11:17:47 +0800

    user model

---
+++ src/main/java/com/sevensevengsi/model/Users.java
@@ -0,0 +1,43 @@
+package com.sevensevengsi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+	@SequenceGenerator(name = "users_seq", initialValue = 1, allocationSize = 1)
+	private Long id;
+	
+	private String username;
+	
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
